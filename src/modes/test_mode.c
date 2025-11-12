259f207e6c1cb990bf2ab40b42c8adb191a93c6f
Vini-boat <vinibezerra2004@gmail.com>
2025-11-12 17:59:22 -0300

    .

--- src/modes/test_mode.c
+++ src/modes/test_mode.c
@@ -24,7 +24,6 @@ void run_test_mode(args_t *args){
     
 
     int reader_thread_error = pthread_create(&reader_thread_id, NULL, run_reader_thread, (void*) reader_thread_args);
-
     if(reader_thread_error != 0) {
         fprintf(stderr, "[ERROR] Criando reader_thread %d\n",reader_thread_error);
         reader_thread_args_destroy(reader_thread_args);
@@ -33,20 +32,21 @@ void run_test_mode(args_t *args){
         
         return;
     }
+    printf("reader_thread_id: %lu\n", reader_thread_id);
     
-    int fifo_to_stdout_thread_error = pthread_create(&fifo_to_stdout_thread_id, NULL, run_fifo_to_stdout_thread, (void*) fifo_to_stdout_thread_args);
-    
+    int fifo_to_stdout_thread_error = pthread_create(&fifo_to_stdout_thread_id, NULL, run_fifo_to_stdout_thread, (void*) fifo_to_stdout_thread_args);    
     if(fifo_to_stdout_thread_error != 0) {
         fprintf(stderr, "[ERROR] Criando fifo_to_stdout_thread %d\n",fifo_to_stdout_thread_error);
-
+        
         pthread_join(reader_thread_id, NULL);
-
+        
         reader_thread_args_destroy(reader_thread_args);
         fifo_to_stdout_thread_args_destroy(fifo_to_stdout_thread_args);
         fifo_destroy(&fifo_read_to_stdout);
         
         return;
     }
+    printf("fifo_to_stdout_thread_id: %lu\n", fifo_to_stdout_thread_id);
 
     void *reader_status;
     void *stdout_status;
